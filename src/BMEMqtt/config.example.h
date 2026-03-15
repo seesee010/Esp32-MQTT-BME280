7178d68ed605d36baa7c37002a6cb2b05c106ead
SeeSee1 <sebastian.daubner2@gmail.com>
2026-03-15 22:08:48 +0100

    Update example configuration for MQTT settings

--- src/BMEMqtt/config.example.h
+++ src/BMEMqtt/config.example.h
@@ -1,10 +1,16 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+// your wifi name
 const char *ssid = "my_wifi";
+
+// wifi passwd
 const char *password = "password";
+
+// mqtt server - where it should send the data
 const char *mqtt_server = "broker.emqx.io";
 
+// mqtt user account - username and passwd
 const char *mqtt_user = "esp32_sensor";
 const char *mqtt_pass = "your_password";
 
